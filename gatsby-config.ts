86fd6f38e51f67129403fedcff1ddcd9f1b9dc36
Rohit Chouhan <ro.chouhan@gmail.com>
2022-05-15 17:31:16 -0700

    Initial commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,30 +1,35 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `Portfolio`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-sass", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": ""
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
-};
+	siteMetadata: {
+		title: `Portfolio`,
+		siteUrl: `https://www.yourdomain.tld`,
+	},
+	plugins: [
+		"gatsby-plugin-image",
+		"gatsby-plugin-react-helmet",
+		"gatsby-plugin-sitemap",
+		"gatsby-plugin-mdx",
+		"gatsby-plugin-sharp",
+		"gatsby-transformer-sharp",
+		"gatsby-plugin-postcss",
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "images",
+				path: "./src/images/",
+			},
+			__key: "images",
+		},
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "pages",
+				path: "./src/pages/",
+			},
+			__key: "pages",
+		},
+	],
+}
 
-export default config;
+export default config
---
+++ src/components/Toolbar.tsx
@@ -0,0 +1,8 @@
+import * as React from "react"
+
+// markup
+const Toolbar = () => {
+	return <div className="p-4 bg-gray-500"></div>
+}
+
+export default Toolbar
