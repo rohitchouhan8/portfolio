8ed0167668e4399e06f3270ebf37fe9d63c439f0
Rohit Chouhan <ro.chouhan@gmail.com>
2022-11-20 18:52:23 -0800

    Style changes

---
+++ src/utils/text.ts
@@ -0,0 +1,8 @@
+export function toSentenceCase(s: string): string {
+	if (!s) {
+		return s
+	}
+	const words = s.trim().split(" ")
+	const firstWord = words[0].charAt(0).toUpperCase() + words[0].slice(1)
+	return [firstWord, ...words.slice(1)].join(" ")
+}
