2c9555238096c398204f53e4d4a089f75c383641
Rohit Chouhan <ro.chouhan@gmail.com>
2022-12-03 17:34:39 -0800

    Client only component

---
+++ src/components/ClientOnly.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function ClientOnly({
+  children,
+  ...delegated
+}: React.PropsWithChildren<{}>) {
+  const [hasMounted, setHasMounted] = React.useState(false)
+  React.useEffect(() => {
+    setHasMounted(true)
+  }, [])
+  if (!hasMounted) {
+    return null
+  }
+  return <span {...delegated}>{children}</span>
+}
