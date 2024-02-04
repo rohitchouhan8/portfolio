55d1c768d3aaf08cfd368df1cc5d55a1203f1d05
Rohit Chouhan <ro.chouhan@gmail.com>
2024-02-03 16:00:46 -0800

    update icon

--- components/FavIcon.tsx
+++ components/FavIcon.tsx
@@ -1,63 +1,13 @@
+import Image from 'next/image';
+
 export default function FavIcon() {
   return (
-    <svg
-      width="32"
-      height="32"
-      viewBox="0 0 32 32"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <mask
-        id="mask0_108_25"
-        style={{ maskType: 'alpha' }}
-        maskUnits="userSpaceOnUse"
-        x="2"
-        y="2"
-        width="28"
-        height="28"
-      >
-        <path
-          fillRule="evenodd"
-          clipRule="evenodd"
-          d="M20.291 2.41342H13.3181C7.39345 2.41342 2.59058 7.5847 2.59058 13.9638V19.739V28.5568C2.59058 29.1091 3.03829 29.5568 3.59058 29.5568H11.709H18.6818C24.6065 29.5568 29.4093 24.3855 29.4093 18.0064V12.2312V3.41342C29.4093 2.86114 28.9616 2.41342 28.4093 2.41342H20.291ZM19.291 12.2312C19.8432 12.2312 20.291 12.679 20.291 13.2312V18.0064C20.291 18.9633 19.5705 19.739 18.6818 19.739H12.709C12.1567 19.739 11.709 19.2913 11.709 18.739V13.9638C11.709 13.0069 12.4294 12.2312 13.3181 12.2312H19.291Z"
-          fill="url(#paint0_linear_108_25)"
-        />
-      </mask>
-      <g mask="url(#mask0_108_25)">
-        <rect
-          x="2.59058"
-          y="2.41342"
-          width="26.8188"
-          height="27.1434"
-          fill="url(#paint1_linear_108_25)"
-        />
-      </g>
-      <defs>
-        <linearGradient
-          id="paint0_linear_108_25"
-          x1="2.59058"
-          y1="29.5568"
-          x2="29.732"
-          y2="2.73998"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stopColor="#EAB308" />
-          <stop offset="0.5" stopColor="#EC538B" />
-          <stop offset="1" stopColor="#EC474A" />
-        </linearGradient>
-        <linearGradient
-          id="paint1_linear_108_25"
-          x1="29.4093"
-          y1="2.41342"
-          x2="2.26792"
-          y2="29.2302"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stopColor="#EC3900" />
-          <stop offset="0.5" stopColor="#EC538B" />
-          <stop offset="1" stopColor="#EAB308" />
-        </linearGradient>
-      </defs>
-    </svg>
+    <Image
+      className="saturate-150 dark:brightness-100 brightness-125"
+      src="/favicon.png"
+      alt="favicon"
+      width={32}
+      height={32}
+    />
   );
 }
