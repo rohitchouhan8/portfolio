08f19f18f93d5d21538b23c3f09fd304447976a6
Rohit Chouhan <ro.chouhan@gmail.com>
2024-05-27 10:35:44 -0700

    Add Pulse Fitness AI page

--- app/projects/pulse-fitness-ai/page.tsx
+++ app/projects/pulse-fitness-ai/page.tsx
@@ -6,12 +6,14 @@ import Image from 'next/image';
 import gif from '../../../public/builder-demo.gif';
 import { list } from '@vercel/blob';
 import { Suspense } from 'react';
+import { Callout } from '@/components/Callout';
 
 export default function PageDetail() {
   return (
     <>
       <ScrollProgress />
       <H1>{pulseFitnessAI.name}.</H1>
+      <Callout variant={'warning'}>This project is no longer live.</Callout>
       <Suspense fallback={<Image src={gif} alt="Pulse Fitness AI" />}>
         <VideoComponent fileName="Pulse Fitness AI Demo-wlHBRO8aFHnKVWwpc86v52d0SvrUiY" />
       </Suspense>
@@ -21,6 +23,24 @@ export default function PageDetail() {
 }
 
 const content = `
+Pulse Fitness AI is a web app that leverages LLMs to generate personalized workout plans.
+
+## Motivation 
+I found existing workout apps to be too generic and not tailored to my fitness goals. Any time I would create a workout plan, I would have to manually adjust the exercises and sets to fit my needs. It negatively impacted my workout experience. 
+
+I found myself frequently using ChatGPT to generate workout plans, which led me to creating Pulse Fitness AI. 
+
+In addition, it was a great opportunity to learn about LLMs and explore various UI formats to interact with AI. 
+
+## Features
+- **Personalized Workout Plans**: Users can generate workout plans tailored to their fitness goals.
+- **Workout Directory**: Users can view and save generated workout plans.
+- **Adjustable Workout Plans**: Users can adjust the generated workout plans to fit their needs.
+
+## Tech stack
+- **Frontend**: NextJS, React, Tailwind CSS, Shadcn UI
+- **Backend / Auth**: Supabase 
+- **AI**: GPT-3.5 (OpenAI)
 `;
 
 async function VideoComponent({ fileName }: { fileName: string }) {
