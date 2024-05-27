58ca2505437e56e24995bc6ee22565d5c8454ae8
Rohit Chouhan <ro.chouhan@gmail.com>
2024-05-27 10:44:24 -0700

    fix project sorting order

--- models/projects/pulse-fitness.ts
+++ models/projects/pulse-fitness.ts
@@ -5,5 +5,5 @@ export const pulseFitnessAI: Project = {
   id: ProjectId.PULSE_FITNESS_AI,
   name: 'Pulse Fitness AI',
   startDate: dayjs('2023-06-01'),
-  endDate: dayjs('2023-12--08'),
+  endDate: dayjs('2023-12-08'),
 };
