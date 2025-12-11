62ac46d66891973919aaebdef539bb32caba14b7
Rohit Chouhan <ro.chouhan@gmail.com>
2025-12-11 07:55:18 -0800

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import { Page } from '@/components/Page';
 import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
+import { Analytics } from '@vercel/analytics/next';
 
 export default function RootLayout({
   children,
@@ -11,6 +12,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
       <body>
+        <Analytics />
         <Page>{children}</Page>
       </body>
     </html>
